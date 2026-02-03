a17a5cf6f4573fb598d20049f0fd257aeb2fafc5
EndlessPixel <3785267350@qq.com>
2026-02-03 12:26:43 +0800

    更新next配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,   // 慎用，仅临时绕过类型错误
-  },
-  images: {
-    unoptimized: true, // 临时禁用图片优化，待修复后删除
-  },
 };
 
 export default nextConfig; // 导出
