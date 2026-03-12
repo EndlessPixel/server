281c0e06a57630140f19c86a2fbe20ac4866cd41
EndlessPixel <3785267350@qq.com>
2026-03-12 20:42:24 +0800

    fix: 修正导入语句中的组件名称大小写

--- app/downloads/launcher/page.tsx
+++ app/downloads/launcher/page.tsx
@@ -1,7 +1,6 @@
-import { LauncherListPage } from "@/components/LauncherListPage";
+import { LauncherListPage } from "@/components/launcher-list-page";
 import { Navigation } from "@/components/navigation";
 import { Footer } from "@/components/footer";
-
 export default function Page() {
   return (
     <>
