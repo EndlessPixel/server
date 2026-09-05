e57b8dd4e415b56e6188f56dca952629d95b48d2
EndlessPixel <system-mini@outlook.com>
2026-09-05 13:28:51 +0800

    chore(eslint): 关闭部分 react-hooks 规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,11 @@ const eslintConfig = [
       "@next/next/no-img-element": "warn",
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/immutability": "off",
+      "react/display-name": "off",
+      "react-hooks/static-components": "off",
     },
   },
   {
